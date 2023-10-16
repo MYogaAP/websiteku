eff1b8210c1eb3440349998712ce9935279b719e
Muhammad Yoga Adha Pratama <2011016110009@mhs.ulm.ac.id>
2023-10-16 19:23:30 +0800

    upload add

--- resources/views/UploadAndView.blade.php
+++ resources/views/UploadAndView.blade.php
@@ -47,17 +47,24 @@
             </div>
             <div class="col text-start">
                 <p>Upload Gambar Iklan</p>
-                <button type="button" class="btn btn-primary px-5 rounded-3">Pilih Gambar</button>
+                <form action="{{route('RegisterCall')}}" method="POST">
+                    @csrf
+                    @method('POST')
+                    <label for="image" class="btn btn-primary px-5 rounded-3">
+                        <input name="image" id="image" type="file" class="visually-hidden">
+                        Pilih Gambar
+                    </label>
+                </form>
+
                 <p class="mt-3">Jenis Warna Iklan</p>
-                <div>
-                    <button type="button" class="btn btn-outline-primary rounded-pill"><i
-                            class="fa-solid fa-circle me-1"></i>Full Warna</button>
-                    <button type="button" class="btn btn-outline-secondary rounded-pill ms-2"><i
-                            class="fa-solid fa-circle me-1"></i>Hitam Putih</button>
-                </div>
+                <input type="radio" class="btn-check" name="options" id="option1" value="fc" readonly>
+                <label class="btn btn-primary" for="option1">Full Warna</label>
+                <input type="radio" class="btn-check" name="options" id="option2" value="bw" readonly>
+                <label class="btn btn-secondary" for="option2">Hitam Putih</label>
+                    
                 <p class="mt-3">Status</p>
                 <div class="border border-black text-center p-3">
-                    <p>gambar dan Jenis warna tidak sesuai
+                    <p>Ukuran tidak sesuai
                         <br> Mohon upload ulang dan hubungi nomor dibawah :
                     </p>
                     <button type="button" class="btn btn-success btn-sm rounded-3">Contact Support <i
@@ -78,6 +85,75 @@
             </div>
         </div>
     </div>
+    
+    <script>
+        document.getElementById('image').addEventListener('change', function() {
+            function getCookie(name) {
+                var cookies = document.cookie.split(';');
+
+                for (var i = 0; i < cookies.length; i++) {
+                    var cookie = cookies[i].trim(); // Remove leading/trailing spaces
+                    var parts = cookie.split('=');
+                    var cookieName = parts[0];
+                    var cookieValue = parts[1];
+
+                    if (cookieName === name) {
+                        return decodeURIComponent(cookieValue); // Decode the value in case it's URL-encoded
+                    }
+                }
+
+                return null; // Return null if the cookie is not found
+            }
+
+            // Usage example
+            var myCookieValue = getCookie('auth');
+            var photo = this.files[0];
+            var packet_id = "";
+
+            var myHeaders = new Headers();
+            myHeaders.append("Accept", "application/json");
+            myHeaders.append("Authorization", "Bearer " + myCookieValue);
+
+            var formdata = new FormData();
+            formdata.append("image", photo);
+            formdata.append("packet_id", 1);
+
+            var requestOptions = {
+                method: 'POST',
+                headers: myHeaders,
+                body: formdata,
+                redirect: 'follow'
+            };
+
+            fetch("127.0.0.1/websiteku/public/api/CheckImage", requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                console.log('Response:', data);
+                // Handle the response as needed
+            })
+            .catch(error => console.log('error', error));
+
+            //////////////////////////////////////////////////
+            var formData = new FormData();
+            formData.append('photo', photo);
+
+            fetch('{{ route('upload.store') }}', {
+                method: 'POST',
+                body: formData,
+                headers: {
+                    'X-CSRF-TOKEN': '{{ csrf_token() }}',
+                }
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Response:', data);
+                // Handle the response as needed
+            })
+            .catch(error => console.error('Error:', error));
+        });
+    </script>
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
