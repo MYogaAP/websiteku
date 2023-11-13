4be2de35a786b7482d7a89448f964dcbcf4eb26a
Muhammad Yoga Adha Pratama <2011016110009@mhs.ulm.ac.id>
2023-11-13 20:10:56 +0800

    fe new 3

--- resources/views/landingPagePro.blade.php
+++ resources/views/landingPagePro.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Periklanan Radar Banjarmasin</title>
+    <title>Jasa Iklan Radar Banjarmasin</title>
     <!-- Favicon-->
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
     <!-- Bootstrap icons-->
@@ -21,6 +21,9 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Bodoni+Moda:opsz,wght@6..96,800&family=Montserrat&display=swap"
         rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" 
+    integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" 
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
     <style>
         header {
             background-image: url({{ asset('customerStyle/background-home.png') }});
@@ -40,6 +43,70 @@
         .judul:hover {
             color: #1450A3;
         }
+
+        .scroll-down-indicator {
+            position: absolute;
+            bottom: 20px;
+            left: 50%;
+            transform: translateX(-50%);
+            animation: fadeInDown 2s ease infinite;
+        }
+
+        .scroll-down-indicator p {
+            margin-bottom: 5px;
+            color: white;
+            text-align: center;
+        }
+
+        .arrow {
+            width: 30px;
+            height: 30px;
+            border-width: 2px 2px 0 0;
+            transform: rotate(-45deg);
+            position: relative;
+            left: 50%;
+            transform: translateX(-50%);
+            text-align: center;
+            animation: bounce 2s infinite;
+        }
+
+        .arrow::before {
+            content: '';
+            position: absolute;
+            width: 15px;
+            height: 15px;
+            border-right: 2px solid white;
+            border-bottom: 2px solid white;
+            transform: rotate(45deg);
+            bottom: 5px;
+            right: 5px;
+        }
+
+        @keyframes fadeInDown {
+            0% {
+                opacity: 0;
+            }
+            50% {
+                opacity: 1;
+            }
+            100% {
+                opacity: 0;
+            }
+        }
+
+        @keyframes bounce {
+            0%, 20%, 50%, 80%, 100% {
+                transform: translateY(0);
+            }
+            40% {
+                transform: translateY(-10px);
+            }
+            60% {
+                transform: translateY(-5px);
+            }
+        }
+
+
     </style>
 </head>
 
@@ -86,8 +153,8 @@
         @endif
 
         <!-- Header-->
-        <header class="py-5">
-            <div class="container px-5">
+        <header class="py-4 position-relative">
+            <div class="container px-4">
                 <div class="row gx-5 align-items-center justify-content-center">
                     <div class="col-lg-8 col-xl-7 col-xxl-6">
                         <div class="my-5 text-center text-xl-start">
@@ -108,43 +175,45 @@
                             src="{{ asset('images/koran.png') }}" alt="..." /></div>
                 </div>
             </div>
+            <div class="scroll-down-indicator">
+                <div class="arrow"></div>
+            </div>
         </header>
         <!-- Features section-->
         <section class="py-5" id="features">
             <div class="container px-5 my-5">
                 <div class="row gx-5">
                     <div class="col-lg-4 mb-5 mb-lg-0 d-flex align-items-center justify-content-center">
-                        <h2 class="fw-bolder mb-5">Kini lebih mudah untuk memesan jasa iklan koran</h2>
+                        <h3 class="fw-bolder mb-5">Keuntungan dan kemudahan jasa iklan koran secara digital</h3>
                     </div>
-                    <div class="col-lg-8">
-                        <div class="row gx-5 row-cols-1 row-cols-md-2">
+                    <div class="col-lg">
+                        <div class="row gx-5 row-cols-1 row-cols-md-3">
                             <div class="col mb-5 h-100">
-                                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i
-                                        class="bi bi-collection"></i></div>
-                                <h2 class="h5">Featured title</h2>
-                                <p class="mb-0">Paragraph of text beneath the heading to explain the heading. Here is
-                                    just a bit more text.</p>
+                                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3">
+                                    <i class="fa-solid fa-handshake-simple"></i>
+                                </div>
+                                <h2 class="h5 fw-bold">Kemudahan Akses</h2>
+                                <p class="mb-0" style="text-align:justify">Proses pemesanan yang dapat dilakukan kapan saja dan di mana saja 
+                                    menghemat waktu dan tenaga Anda. Tidak perlu lagi datang langsung ke kantor 
+                                    untuk melakukan transaksi, memberikan fleksibilitas yang signifikan.</p>
                             </div>
                             <div class="col mb-5 h-100">
-                                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i
-                                        class="bi bi-building"></i></div>
-                                <h2 class="h5">Featured title</h2>
-                                <p class="mb-0">Paragraph of text beneath the heading to explain the heading. Here is
-                                    just a bit more text.</p>
-                            </div>
-                            <div class="col mb-5 mb-md-0 h-100">
-                                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i
-                                        class="bi bi-toggles2"></i></div>
-                                <h2 class="h5">Featured title</h2>
-                                <p class="mb-0">Paragraph of text beneath the heading to explain the heading. Here is
-                                    just a bit more text.</p>
+                                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3">
+                                    <i class="fa-brands fa-get-pocket"></i>
+                                </div>
+                                <h2 class="h5 fw-bold">Fleksibilitas Pemilihan Paket</h2>
+                                <p class="mb-0" style="text-align:justify">Tanpa proses rumit, sekarang Anda dapat menyesuaikan pilihan iklan sesuai 
+                                    dengan target audiens dan anggaran bisnis Anda. Hal ini memberikan tingkat 
+                                    fleksibilitas yang optimal untuk strategi pemasaran Anda.</p>
                             </div>
                             <div class="col h-100">
-                                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i
-                                        class="bi bi-toggles2"></i></div>
-                                <h2 class="h5">Featured title</h2>
-                                <p class="mb-0">Paragraph of text beneath the heading to explain the heading. Here is
-                                    just a bit more text.</p>
+                                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3">
+                                    <i class="fa-solid fa-hand-holding-dollar"></i>
+                                </div>
+                                <h2 class="h5 fw-bold">Transaksi Secara Digital</h2>
+                                <p class="mb-0" style="text-align:justify">Pembayaran dapat dilakukan secara digital, menghilangkan kebutuhan 
+                                    untuk transaksi tunai atau pergi ke lokasi fisik, memberikan tingkat keamanan 
+                                    dan kenyamanan yang tinggi.</p>
                             </div>
                         </div>
                     </div>
@@ -155,17 +224,16 @@
         <div class="py-5 bg-light">
             <div class="container px-5 my-5">
                 <div class="row gx-5 justify-content-center">
-                    <div class="col-lg-10 col-xl-7">
+                    <div class="col">
                         <div class="text-center">
-                            <div class="fs-4 mb-4 fst-italic">"Ciptakan jejak sukses bisnismu melalui iklan koran yang
-                                tepat sasaran."</div>
+                            <div class="fs-4 mb-5 fst-italic">"Ciptakan jejak sukses bisnismu melalui iklan koran yang tepat sasaran."</div>
                             <div class="d-flex align-items-center justify-content-center">
                                 <img class="rounded-circle me-3" src="https://dummyimage.com/40x40/ced4da/6c757d"
                                     alt="..." />
                                 <div class="fw-bold">
                                     Radar Banjarmasin
-                                    <span class="fw-bold text-primary mx-1">/</span>
-                                    CEO, Pomodoro
+                                    <span class="fw-bold text-primary mx-1">-</span>
+                                    Paling Paham Soal Banua
                                 </div>
                             </div>
                         </div>
@@ -173,144 +241,17 @@
                 </div>
             </div>
         </div>
-        <!-- Blog preview section-->
-        <section class="py-5">
-            <div class="container px-5 my-5">
-                <div class="row gx-5 justify-content-center">
-                    <div class="col-lg-8 col-xl-6">
-                        <div class="text-center">
-                            <h2 class="fw-bolder">From our blog</h2>
-                            <p class="lead fw-normal text-muted mb-5">Lorem ipsum, dolor sit amet consectetur
-                                adipisicing elit. Eaque fugit ratione dicta mollitia. Officiis ad.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="row gx-5">
-                    <div class="col-lg-4 mb-5">
-                        <div class="card h-100 shadow border-0">
-                            <img class="card-img-top" src="https://dummyimage.com/600x350/ced4da/6c757d"
-                                alt="..." />
-                            <div class="card-body p-4">
-                                <div class="badge bg-primary bg-gradient rounded-pill mb-2">News</div>
-                                <a class="text-decoration-none link-dark stretched-link" href="#!">
-                                    <h5 class="card-title mb-3">Blog post title</h5>
-                                </a>
-                                <p class="card-text mb-0">Some quick example text to build on the card title and
-                                    make
-                                    up the bulk of the card's content.</p>
-                            </div>
-                            <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
-                                <div class="d-flex align-items-end justify-content-between">
-                                    <div class="d-flex align-items-center">
-                                        <img class="rounded-circle me-3"
-                                            src="https://dummyimage.com/40x40/ced4da/6c757d" alt="..." />
-                                        <div class="small">
-                                            <div class="fw-bold">Kelly Rowan</div>
-                                            <div class="text-muted">March 12, 2023 &middot; 6 min read</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 mb-5">
-                        <div class="card h-100 shadow border-0">
-                            <img class="card-img-top" src="https://dummyimage.com/600x350/adb5bd/495057"
-                                alt="..." />
-                            <div class="card-body p-4">
-                                <div class="badge bg-primary bg-gradient rounded-pill mb-2">Media</div>
-                                <a class="text-decoration-none link-dark stretched-link" href="#!">
-                                    <h5 class="card-title mb-3">Another blog post title</h5>
-                                </a>
-                                <p class="card-text mb-0">This text is a bit longer to illustrate the adaptive
-                                    height
-                                    of each card. Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.</p>
-                            </div>
-                            <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
-                                <div class="d-flex align-items-end justify-content-between">
-                                    <div class="d-flex align-items-center">
-                                        <img class="rounded-circle me-3"
-                                            src="https://dummyimage.com/40x40/ced4da/6c757d" alt="..." />
-                                        <div class="small">
-                                            <div class="fw-bold">Josiah Barclay</div>
-                                            <div class="text-muted">March 23, 2023 &middot; 4 min read</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 mb-5">
-                        <div class="card h-100 shadow border-0">
-                            <img class="card-img-top" src="https://dummyimage.com/600x350/6c757d/343a40"
-                                alt="..." />
-                            <div class="card-body p-4">
-                                <div class="badge bg-primary bg-gradient rounded-pill mb-2">News</div>
-                                <a class="text-decoration-none link-dark stretched-link" href="#!">
-                                    <h5 class="card-title mb-3">The last blog post title is a little bit longer
-                                        than
-                                        the others</h5>
-                                </a>
-                                <p class="card-text mb-0">Some more quick example text to build on the card title
-                                    and
-                                    make up the bulk of the card's content.</p>
-                            </div>
-                            <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
-                                <div class="d-flex align-items-end justify-content-between">
-                                    <div class="d-flex align-items-center">
-                                        <img class="rounded-circle me-3"
-                                            src="https://dummyimage.com/40x40/ced4da/6c757d" alt="..." />
-                                        <div class="small">
-                                            <div class="fw-bold">Evelyn Martinez</div>
-                                            <div class="text-muted">April 2, 2023 &middot; 10 min read</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                {{-- <!-- Call to action-->
-                <aside class="bg-primary bg-gradient rounded-3 p-4 p-sm-5 mt-5">
-                    <div
-                        class="d-flex align-items-center justify-content-between flex-column flex-xl-row text-center text-xl-start">
-                        <div class="mb-4 mb-xl-0">
-                            <div class="fs-3 fw-bold text-white">New products, delivered to you.</div>
-                            <div class="text-white-50">Sign up for our newsletter for the latest updates.</div>
-                        </div>
-                        <div class="ms-xl-4">
-                            <div class="input-group mb-2">
-                                <input class="form-control" type="text" placeholder="Email address..."
-                                    aria-label="Email address..." aria-describedby="button-newsletter" />
-                                <button class="btn btn-outline-light" id="button-newsletter" type="button">Sign
-                                    up</button>
-                            </div>
-                            <div class="small text-white-50">We care about privacy, and will never share your data.
-                            </div>
-                        </div>
-                    </div>
-                </aside> --}}
-            </div>
-        </section>
     </main>
     <!-- Footer-->
-    {{-- <footer class="bg-dark py-4 mt-auto">
+    <footer class="bg-dark py-4 mt-auto">
         <div class="container px-5">
             <div class="row align-items-center justify-content-between flex-column flex-sm-row">
                 <div class="col-auto">
-                    <div class="small m-0 text-white">Copyright &copy; Your Website 2023</div>
-                </div>
-                <div class="col-auto">
-                    <a class="link-light small" href="#!">Privacy</a>
-                    <span class="text-white mx-1">&middot;</span>
-                    <a class="link-light small" href="#!">Terms</a>
-                    <span class="text-white mx-1">&middot;</span>
-                    <a class="link-light small" href="#!">Contact</a>
+                    <div class="small m-0 text-white">Copyright &copy; Kerja Praktik - Universitas Lambung Mangkurat 2023</div>
                 </div>
             </div>
         </div>
-    </footer> --}}
+    </footer>
     <!-- Bootstrap core JS-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Core theme JS-->
