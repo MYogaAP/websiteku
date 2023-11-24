b4642eaa0317225a7871c68675838c98c86a2db9
TapZe <nabilmuyassar@Gmail.com>
2023-11-24 19:48:49 +0800

    icon

--- resources/views/AgentData.blade.php
+++ resources/views/AgentData.blade.php
@@ -10,6 +10,9 @@
 
     <title>Admin Dashboard - Radar Banjarmasin</title>
 
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="{{asset('favicon.ico')}}" />
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
