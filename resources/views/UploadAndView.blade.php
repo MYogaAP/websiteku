5245ae9ae1902bc8bc8263f1c1092b72423176df
TapZe <nabilmuyassar@Gmail.com>
2023-11-25 18:04:09 +0800

    Update UploadAndView.blade.php

--- resources/views/UploadAndView.blade.php
+++ resources/views/UploadAndView.blade.php
@@ -79,7 +79,7 @@
 
             $curl = curl_init();
             curl_setopt_array($curl, array(
-            CURLOPT_URL => '127.0.0.1/websiteku/public/api/CheckImage',
+            CURLOPT_URL => gethostname() .'/websiteku/public/api/CheckImage',
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
